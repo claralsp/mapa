5aa530195fb651f50699414d9e1288ffb73331d2
Unknown <claraalsp@gmail.com>
2021-01-09 19:18:50 -0300

    Bug botões

--- scripts/rodape.js
+++ scripts/rodape.js
@@ -1,21 +1,16 @@
-// pega todos os elementos do DOM com a classe 'boo'
 let btnRdps= document.querySelectorAll('.btnRdp');
 const bntRodape1 = document.querySelector('.btnRdp1');
 const bntRodape2 = document.querySelector('.btnRdp2');
 const bntRodape3 = document.querySelector('.btnRdp3');
 
 
-// percorre a lista de 'boo's
 for (let i = 0; i < btnRdps.length; i++) {
-	// atribui um evento para o 'boo' atual
 	btnRdps[i].addEventListener('click', function(e) {
   
-  	// remove a classe 'selecionado' de todos os 'boos'
     for (let j = 0; j < btnRdps.length; j++) {
     	btnRdps[j].classList.remove('btnRdpClicado');
     };
     
-    // recoloca a classe 'selecionado' no 'boo' que foi clicado
   	let btnRdpEl = e.currentTarget;
     btnRdpEl.classList.add('btnRdpClicado');
     
