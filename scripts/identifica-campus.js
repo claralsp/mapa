5b5f75ab1e4fce45a07d7aa61f047559b357cb77
AliceGazzinelli <alice.mendesgazzinelli@gmail.com>
2021-01-10 20:02:19 -0300

    Create identifica-campus.js

---
+++ scripts/identifica-campus.js
@@ -0,0 +1,39 @@
+navigator.geolocation.getCurrentPosition(localizarCampus);
+
+let campi = [
+    {
+        nome: "campus1",
+        latMinima: -19.930157556478836,
+        latMaxima: -19.93020565209033,
+        longMinima: -43.97588977591026,
+        longMaxima: -43.979443789664685
+    },
+    {
+        nome: "campus2",
+        latMinima: -19.938062048585753,
+        latMaxima: -19.940278127250064,
+        longMinima: -43.997893198166366,
+        longMaxima: -44.000726569341225
+    }
+]
+
+function localizarCampus(geoLocalizacao) {
+    let x = (geoLocalizacao.coords.latitude);
+    let y = (geoLocalizacao.coords.longitude);
+    let userDentroCampus = false;
+    let campusUser = null;
+  
+    for(let campus of campi) {
+        userDentroCampus = x>=campus.latMinima && x<=campus.latMaxima && y>=campus.longMinima && y<=campus.longMaxima;
+        if(userDentroCampus) {
+          campusUser = campus;
+          console.log("Voce esta no " + campusUser.nome);
+          break;
+        }       
+    }
+    
+    if(!userDentroCampus) {
+        console.log("Voce nao esta em nenhum campus");
+    }
+  
+}
