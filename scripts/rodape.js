56886af2275b0b78c9993f3110922883f9644621
Unknown <claraalsp@gmail.com>
2021-01-10 14:05:48 -0300

    Bug 1.2

--- scripts/rodape.js
+++ scripts/rodape.js
@@ -1,40 +1,67 @@
-let btnRdps= document.querySelectorAll('.btnRdp');
+const b1 = document.getElementById('btn1');
+const b2 = document.getElementById('btn2');
+
 const bntRodape1 = document.querySelector('.btnRdp1');
-const bntRodape2 = document.querySelector('.btnRdp2');
-const bntRodape3 = document.querySelector('.btnRdp3');
+var bntRodape1Clicado = false;
 
-
-for (let i = 0; i < btnRdps.length; i++) {
-	btnRdps[i].addEventListener('click', function(e) {
+bntRodape1.addEventListener('click', () => {
+if(bntRodape1Clicado == false) {
+  bntRodape1.classList.add('btnRdpClicado');
+  document.getElementById('mapeamento').style.display = 'none';
+  document.getElementById('comoChegar').style.display = 'block';
   
-    for (let j = 0; j < btnRdps.length; j++) {
-    	btnRdps[j].classList.remove('btnRdpClicado');
-    };
-    
-  	let btnRdpEl = e.currentTarget;
-    btnRdpEl.classList.add('btnRdpClicado');
-    
-
-  if(bntRodape1 == bntRodape1Clicado) {
-    bntRodape1.classList.add('btnRdpClicado');
-    document.getElementById('comoChegar').style.display = 'block';
-  }
-
-  if(bntRodape2 == bntRodape2Clicado) {
-    bntRodape2.classList.add('btnRdpClicado');
-    bntRodape2Clicado = true;
-    document.getElementById('secao-busca').style.display = 'block';
-    return;
-  }
-  
-  if(bntRodape3 == bntRodape3Clicado) {
-      document.getElementById('mapeamento').style.display = 'block';
-      bntRodape3Clicado = true;
-      return;
-    }
+  b1.addEventListener('click', () =>{
+    document.getElementById('comoChegarCampusII').style.display = 'none';
+    document.getElementById('comoChegarCampusI').style.display = 'block';
   });
-};
+
+  b2.addEventListener('click', () =>{
+    document.getElementById('comoChegarCampusI').style.display = 'none';
+    document.getElementById('comoChegarCampusII').style.display = 'block';
+  });
+
+  bntRodape1Clicado = true;
+  return;
+}
+else{
+  bntRodape1.classList.remove('btnRdpClicado');
+  document.getElementById('comoChegar').style.display = 'none';
+  document.getElementById('mapeamento').style.display = 'block';
+  bntRodape1Clicado = false;
+}
+});
+
+const bntRodape2 = document.querySelector('.btnRdp2');
+var bntRodape2Clicado = false;
+
+bntRodape2.addEventListener('click', () => {
+if(bntRodape2Clicado == false) {
+  bntRodape2.classList.add('btnRdpClicado');
+  bntRodape2Clicado = true;
+  document.getElementById('secao-busca').style.display = 'block';
+  return;
+}
+else{
+  bntRodape2.classList.remove('btnRdpClicado');
+  document.getElementById('secao-busca').style.display = 'none';
+  bntRodape2Clicado = false;
+}
+});
 
 
+const bntRodape3 = document.querySelector('.btnRdp3');
+var bntRodape3Clicado = false;
 
-
+bntRodape3.addEventListener('click', () => {
+if(bntRodape3Clicado == false) {
+  bntRodape3.classList.add('btnRdpClicado');
+  document.getElementById('mapeamento').style.display = 'block';
+  bntRodape3Clicado = true;
+  return;
+}
+else{
+  bntRodape3.classList.remove('btnRdpClicado');
+  document.getElementById('mapeamento').style.display = 'none';
+  bntRodape3Clicado = false;
+}
+});
