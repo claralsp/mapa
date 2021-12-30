401b6ff59f4a59012096a3f444d27db1191168fb
Flávio Coutinho <fegemo@gmail.com>
2021-12-30 12:42:05 -0300

    Fix: network-first service worker
    Alteração feita para ficar fácil
    evitar que o usuário fique preso
    com uma versão antiga da
    aplicação

--- sw.js
+++ sw.js
@@ -1,81 +1,98 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('mcefet').then((cache) => cache.addAll([     
-      // html
-      '/mapa/',
-      '/mapa/index.html',
-      '/mapa/sobre.html',
-      '/mapa/mcefet.webmanifest',
+const NOME_DO_CACHE = 'mcefet'
+const PREFIXO_DE_CAMINHO = location.pathname.substring(location.pathname.indexOf('/sw.js'))
+const ALL_FILES = [
+  // html
+  '/',
+  '/index.html',
+  '/sobre.html',
+  '/mcefet.webmanifest',
 
-      // css
-      '/mapa/estilos.css',
-      '/mapa/fonts.css',
-      '/mapa/pino-gps.css',
+  // // css
+  '/estilos.css',
+  '/fonts.css',
+  '/pino-gps.css',
 
-      // js
-      '/mapa/scripts/hammer.min.js',
-      '/mapa/scripts/identifica-campus.js',
-      '/mapa/scripts/localizacao-usuario.js',
-      '/mapa/scripts/mapacheckbox.js',
-      '/mapa/scripts/mapeamento-c2.js',
-      '/mapa/scripts/pesquisa.js',
-      '/mapa/scripts/rodape.js',
-      '/mapa/scripts/sobreposicao.js',
-      '/mapa/scripts/tela-sempre-ligada.js',
-      '/mapa/scripts/trabalhar-offline.js',
-      '/mapa/scripts/zoom-no-mapa.js',
+  // js
+  '/scripts/hammer.min.js',
+  '/scripts/identifica-campus.js',
+  '/scripts/localizacao-usuario.js',
+  '/scripts/mapacheckbox.js',
+  '/scripts/mapeamento-c2.js',
+  '/scripts/pesquisa.js',
+  '/scripts/rodape.js',
+  '/scripts/sobreposicao.js',
+  '/scripts/tela-sempre-ligada.js',
+  '/scripts/trabalhar-offline.js',
+  '/scripts/zoom-no-mapa.js',
 
-      // imagens
-      '/mapa/images/c1/banheiros.png',
-      '/mapa/images/c1/base.png',
-      '/mapa/images/c1/caixas-eletronicos.png',
-      '/mapa/images/c1/lugares-para-comer.png',
-      '/mapa/images/c1/lugares-para-descansar.png',
-      '/mapa/images/c1/predios-e-entradas.png',
-      '/mapa/images/c2/banheiros.png',
-      '/mapa/images/c2/base.png',
-      '/mapa/images/c2/caixas-eletronicos.png',
-      '/mapa/images/c2/lugares-para-comer.png',
-      '/mapa/images/c2/lugares-para-descansar2.png',
-      '/mapa/images/c2/predios-e-entradas2.png',
-      '/mapa/images/fotos/coutinho.webp',
-      '/mapa/images/fotos/perfil.jpg',
-      // // '/mapa/images/fotos/.png',
-      // // '/mapa/images/fotos/.png',
-      // // '/mapa/images/fotos/.png',
-      // // '/mapa/images/fotos/.png',
-      // // '/mapa/images/fotos/.png',
-      // // '/mapa/images/fotos/.png',
-      '/mapa/images/cabecalho.png',
-      '/mapa/images/comochegar.png',
-      '/mapa/images/fav.png',
-      '/mapa/images/gps-icone-cinza.webp',
-      '/mapa/images/gps-icone-dourado.webp',
-      '/mapa/images/lugaresnocampus.png',
-      '/mapa/images/mapacampus.png',
-      '/mapa/images/menu.png',
-      '/mapa/images/social-media.png',
+  // imagens
+  '/images/c1/banheiros.png',
+  '/images/c1/base.png',
+  '/images/c1/caixas-eletronicos.png',
+  '/images/c1/lugares-para-comer.png',
+  '/images/c1/lugares-para-descansar.png',
+  '/images/c1/predios-e-entradas.png',
+  '/images/c2/banheiros.png',
+  '/images/c2/base.png',
+  '/images/c2/caixas-eletronicos.png',
+  '/images/c2/lugares-para-comer.png',
+  '/images/c2/lugares-para-descansar2.png',
+  '/images/c2/predios-e-entradas2.png',
+  '/images/fotos/coutinho.webp',
+  '/images/fotos/perfil.jpg',
+  // // '/images/fotos/.png',
+  // // '/images/fotos/.png',
+  // // '/images/fotos/.png',
+  // // '/images/fotos/.png',
+  // // '/images/fotos/.png',
+  // // '/images/fotos/.png',
+  '/images/cabecalho.png',
+  '/images/comochegar.png',
+  '/images/fav.png',
+  '/images/gps-icone-cinza.webp',
+  '/images/gps-icone-dourado.webp',
+  '/images/lugaresnocampus.png',
+  '/images/mapacampus.png',
+  '/images/menu.png',
+  '/images/social-media.png',
 
-      // fontes
-      '/mapa/fonts/fontawesome-social-webfont.eot',
-      '/mapa/fonts/fontawesome-social-webfont.svg',
-      '/mapa/fonts/fontawesome-social-webfont.ttf',
-      '/mapa/fonts/fontawesome-social-webfont.woff',
-      '/mapa/fonts/fontawesome-webfont.eot',
-      '/mapa/fonts/fontawesome-webfont.svg',
-      '/mapa/fonts/fontawesome-webfont.ttf',
-      '/mapa/fonts/fontawesome-webfont.woff',
-      '/mapa/fonts/FontAwesome.otf',
-      '/mapa/amethyst/Amethyst.ttf'
-    ]))
-    .catch(error => {
-      console.error(`Error inside sw.js: ${error}`)
-    })
-  );
-});
+  // fontes
+  '/fonts/fontawesome-social-webfont.eot',
+  '/fonts/fontawesome-social-webfont.svg',
+  '/fonts/fontawesome-social-webfont.ttf',
+  '/fonts/fontawesome-social-webfont.woff',
+  '/fonts/fontawesome-webfont.eot',
+  '/fonts/fontawesome-webfont.svg',
+  '/fonts/fontawesome-webfont.ttf',
+  '/fonts/fontawesome-webfont.woff',
+  '/fonts/FontAwesome.otf',
+  '/amethyst/Amethyst.ttf'
+].map(path => PREFIXO_DE_CAMINHO + path)
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-});
+
+async function fetchThenCache(e) {
+  try {
+    // tenta usar a rede primeiro
+    const [resposta, cache] = await Promise.all([fetch(e.request), caches.open(NOME_DO_CACHE)])
+    await cache.put(e.request.url, resposta.clone())
+    return resposta
+
+  } catch (erro) {
+    console.info('Problema no fetch de ', e.request, ': ', erro)
+    return await caches.match(e.request)
+  }
+}
+
+async function clearCache() {
+  const keys = await caches.keys()
+  return await Promise.all(keys.map(key => caches.delete(key)))
+}
+
+self.addEventListener('fetch', async e => {
+  e.respondWith(fetchThenCache(e))
+})
+
+self.addEventListener('activate', async e => {
+  e.waitUntil(clearCache())
+  return self.clients.claim() 
+})
